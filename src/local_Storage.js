46a683c775f885f56b70f7a9a96730f9a05270f3
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-07-15 22:42:50 +0100

    Fix linter errors

--- src/local_Storage.js
+++ src/local_Storage.js
@@ -2,7 +2,7 @@ class Storage {
   setLocalStorage = (tasks) => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
- 
+
   getLocalStorage = () => (localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : []);
- }
- export default Storage;
+}
+export default Storage;
