cf440157511764822f0b2c3ce6b0e6be6b7c44a0
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-07-15 16:54:08 +0100

    Add Local storage file.

---
+++ src/local_Storage.js
@@ -0,0 +1,8 @@
+class Storage {
+ setLocalStorage = (tasks) => {
+   localStorage.setItem('tasks', JSON.stringify(tasks));
+ }
+
+ getLocalStorage = () => (localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : []);
+}
+export default Storage;
